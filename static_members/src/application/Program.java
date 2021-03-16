04e39d0064ac0ef5977132829d7c0a4961dd0903
Abrão Astério Junior <abcorinthians@gmail.com>
2021-03-16 03:06:27 -0300

    Demo of Static Members: Methods in the program class itself

---
+++ static_members/src/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static final double PI = 3.14159;
+
+	public static void main(String[] args) {
+
+		/*
+		 * Make a program to read any numerical value, and then show how much would be
+		 * the value of a circumference and the volume of a sphere for a radius of that
+		 * value. Also inform the PI value to two decimal places.
+		 */
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter radius: ");
+		double radius = sc.nextDouble();
+
+		double c = circumference(radius);
+		double v = volume(radius);
+
+		System.out.printf("Circumference: %.2f%n", c);
+		System.out.printf("Volume: %.2f%n", v);
+		System.out.printf("PI value: %.2f", PI);
+
+		sc.close();
+	}
+
+	public static double circumference(double radius) {
+		return 2.0 * PI * radius;
+	}
+
+	public static double volume(double radius) {
+		return PI * Math.pow(radius, 3) * 4 / 3;
+	}
+}
