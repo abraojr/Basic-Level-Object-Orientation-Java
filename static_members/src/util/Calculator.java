85fd94d2dd994c708611015a5b7fb1d528515b10
Abrão Astério Junior <abcorinthians@gmail.com>
2021-03-16 03:28:51 -0300

    Demo of Static Members: Calculator class with static method

--- static_members/src/util/Calculator.java
+++ static_members/src/util/Calculator.java
@@ -2,13 +2,13 @@ package util;
 
 public class Calculator {
 
-	public final double PI = 3.14159;
+	public static final double PI = 3.14159;
 
-	public double circumference(double radius) {
+	public static double circumference(double radius) {
 		return 2.0 * PI * radius;
 	}
 
-	public double volume(double radius) {
+	public static double volume(double radius) {
 		return PI * Math.pow(radius, 3) * 4.0 / 3.0;
 	}
 }
