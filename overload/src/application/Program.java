a93ca5084291aa735e3f3c8d25c4dc22dc0c985f
Abrão Astério Junior <abcorinthians@gmail.com>
2021-03-17 22:15:15 -0300

    Demo of overloading: constructors overloading

---
+++ overload/src/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Product;
+
+public class Program {
+	public static void main(String[] args) {
+
+		/*
+		 * Make a program to read the data of a product in stock (name, price and
+		 * quantity in stock). Then:
+		 * • Show product data (name, price, quantity in stock, total value in stock) 
+		 * • Make a stock entry and show the product data again  
+		 * • Make a check in the stock and show the product data again
+		 */
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Price: ");
+		double price = sc.nextDouble();
+		Product product = new Product(name, price);
+
+		System.out.println("\nProduct data: " + product);
+		System.out.print("\nEnter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+
+		System.out.println("\nUpdated data: " + product);
+		System.out.print("\nEnter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+
+		System.out.println("\nUpdated data: " + product);
+		sc.close();
+	}
+}
